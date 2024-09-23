57c2aee5c442738d8ce028cb59d98e0168ad6e1e
Piratha <piratha250392@yahoo.com>
2024-09-23 13:48:39 +0530

    ok

---
+++ src/main/java/com/example/My_Mate/MyMateApplication.java
@@ -0,0 +1,14 @@
+package com.example.My_Mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMateApplication.class, args);
+		System.out.println("System started ...");
+	}
+
+}
---
+++ src/test/java/com/example/My_Mate/MyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.My_Mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
